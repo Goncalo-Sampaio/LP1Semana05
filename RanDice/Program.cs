cfcbaa11f9740adba7676c5e9ec6956a5ab8427c
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-22 16:49:59 +0000

    Parsing the arguments, using them to generate random numbers and showing the sum of that generation, RanDice project

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -6,7 +6,17 @@ namespace RanDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int seed = int.Parse(args[1]);
+            int dice = int.Parse(args[0]);
+            Random random = new Random(seed);
+            int final = 0;
+
+            for (int i = 0; i < dice; i++){
+                final += random.Next(1, 7);
+                Console.WriteLine(final);
+            }
+
+            Console.WriteLine(final);
         }
     }
 }
