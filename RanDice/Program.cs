01dd9f1f46529c60504a1d0fab4c565a080ebdcd
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-22 16:51:34 +0000

    Adding comments to the RanDice program

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -6,16 +6,19 @@ namespace RanDice
     {
         static void Main(string[] args)
         {
+            //parsing the arguments and instantiating a new Random
             int seed = int.Parse(args[1]);
             int dice = int.Parse(args[0]);
             Random random = new Random(seed);
             int final = 0;
 
+            //Going through each dice and making a sum of each number
             for (int i = 0; i < dice; i++){
                 final += random.Next(1, 7);
                 Console.WriteLine(final);
             }
 
+            //Showing the result to the user
             Console.WriteLine(final);
         }
     }
