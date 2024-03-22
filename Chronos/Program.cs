3770620e607c0c6e3b7da66461f1c3fabbd9d455
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-22 15:48:56 +0000

    Formatting the time ellapsed and showing it to the user, Chronos project

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Chronos
@@ -20,6 +21,17 @@ namespace Chronos
 
             crono1.Stop();
             crono2.Stop();
+
+            TimeSpan ts = crono1.Elapsed;
+            TimeSpan ts2 = crono2.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:0}.{1:000}", ts.Seconds,
+                ts.Milliseconds);
+            string elapsedTime2 = String.Format("{0:0}.{1:000}", ts2.Seconds,
+                ts2.Milliseconds);
+            Console.WriteLine(elapsedTime);
+            Console.WriteLine(elapsedTime2);
         }
     }
 }
