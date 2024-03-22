3362e16c6e75b15f68cfe9ad0e0763fe2ba2b198
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-22 17:32:16 +0000

    Added comments to MyGame project

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -9,6 +9,7 @@ namespace MyGame
             int nOfEnemies = int.Parse(args[0]);
             Enemy[] enemies = new Enemy[nOfEnemies];
 
+            //Asking the user for the name of each new enemy
             for (int i = 0; i < nOfEnemies; i++){
                 Console.Write($"Nome do inimigo {i + 1}: ");
                 string name = Console.ReadLine();
@@ -16,6 +17,7 @@ namespace MyGame
                 enemies[i] = enemy;
             }
 
+            //Showing the user every new enemy
             foreach(Enemy x in enemies){
                 string name = x.GetName();
                 float health = x.GetHealth();
