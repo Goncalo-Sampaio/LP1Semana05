6f0de77bca14f6815796d3509476ab32ed7e9847
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-22 17:31:19 +0000

    Asking for enemy info input from user and showing it back, MyGame project

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,22 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int nOfEnemies = int.Parse(args[0]);
+            Enemy[] enemies = new Enemy[nOfEnemies];
+
+            for (int i = 0; i < nOfEnemies; i++){
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string name = Console.ReadLine();
+                Enemy enemy = new Enemy(name);
+                enemies[i] = enemy;
+            }
+
+            foreach(Enemy x in enemies){
+                string name = x.GetName();
+                float health = x.GetHealth();
+                float shield = x.GetShield();
+                Console.WriteLine($"{name} {health} {shield}");
+            }
         }
     }
 }
