e0a617bee18e314ef7d60027f7ef09593add53c8
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-22 15:52:36 +0000

    Adding comments and XML doc to Chronos project

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -4,13 +4,19 @@ using System.Threading;
 
 namespace Chronos
 {
+    /// <summary>
+    /// Program that creates stopwatches, starts them at different times and
+    /// shows the user the elapsed time
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            //Instantiating new stopwatches
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
 
+            //Starting stopwatches and stopping the thread for some time
             crono1.Start();
 
             Thread.Sleep(600);
@@ -19,13 +25,13 @@ namespace Chronos
 
             Thread.Sleep(200);
 
+            //Stopping the stopwatches
             crono1.Stop();
             crono2.Stop();
 
+            //Getting the time elapsed, formatting it and showing it to the user
             TimeSpan ts = crono1.Elapsed;
             TimeSpan ts2 = crono2.Elapsed;
-
-            // Format and display the TimeSpan value.
             string elapsedTime = String.Format("{0:0}.{1:000}", ts.Seconds,
                 ts.Milliseconds);
             string elapsedTime2 = String.Format("{0:0}.{1:000}", ts2.Seconds,
